c5060a41411ba34847e6d233b071bafc6a45e7ad
Evandro Tomaz <tmzplaydev@gmail.com>
2024-01-05 03:58:07 -0300

    Dark mode navbar color fix

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -3,7 +3,7 @@ import { ModeToggler } from "./ModeToggler";
 
 const Header = () => {
   return (
-    <header className="w-full flex items-center fixed py-3 bg-slate-100 z-10">
+    <header className="w-full flex items-center fixed py-3 bg-slate-100 dark:bg-zinc-900 z-10">
       <div className="container flex justify-between">
         <div className="font-heading text-3xl text-primary">Evoware</div>
         <span className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium flex items-center">
